3e55ec436cdb9100052c9b6e037c19b4e144b62a
Anton Ageev <antage@gmail.com>
2022-07-04 04:01:04 +0300

    Add LISTEN_HOST, LISTEN_PORT variables.

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Just run `make`.
 ## Environment variables
 
 * `MEMCACHE_SIZE` - maximal memory size in megabytes (64 by default)
+* `LISTEN_HOST` - listen address (0.0.0.0 by default)
+* `LISTEN_PORT` - listen port (11211 by default)
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'memcached' ]; then
     echo "Starting Memcached in foreground."
-    exec gosu nobody:nogroup /usr/bin/memcached -m ${MEMCACHE_SIZE:-64} -l 0.0.0.0 -p 11211
+    exec gosu nobody:nogroup /usr/bin/memcached -m ${MEMCACHE_SIZE:-64} -l ${LISTEN_HOST:-0.0.0.0} -p ${LISTEN_PORT:-11211}
 fi
 
 exec "$@"
